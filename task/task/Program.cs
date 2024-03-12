ddc8647a437ef661e17f9d63cbdc14250253d5d3
nazeani <tu7pfrcay@code.edu.az>
2024-03-12 04:28:58 +0400

    array

---
+++ task/task/Program.cs
@@ -0,0 +1,65 @@
+﻿#region task1
+//int[] a = { 4, 5, 1, 7, 8 };
+//int min = a[0];
+//for (int i = 0; i < a.Length; i++)
+//{
+//    if (min >= a[i])
+//    {
+//        min = a[i];
+//    }
+//}
+//Console.WriteLine(min);
+#endregion
+#region task2
+//string[] ar2 = { "code", "soz", "naz" };
+//string ccc;
+//for (int i = 0; i < ar2.Length; i++)
+//{  
+//    ccc ="   ";   
+//    for (int j = ar2[i].Length - 1; j >= 0; j--)
+//    {
+//        ccc+= ar2[i][j];
+//    }
+//    Console.WriteLine(ccc);
+//}
+#endregion
+#region task3
+string[] ar3 = { "soz1", "soz2", "soz3" };
+string ters="";
+for (int i = ar3.Length - 1; i >= 0; i--)
+{
+    ters = " " + ar3[i];
+   
+}
+Console.WriteLine(ters);
+#endregion
+#region task4
+//int[] ar4 = { 4,-6,-5,7};
+//int ar42;
+
+
+//for (int i = ar4.Length - 1; i >= 0; i--)
+//{
+//    if (ar4[i] <= 0)
+//    {
+//        ar4[i] *= -1;
+//        string ters = " " + ar4[i];
+//        Console.WriteLine(ters);
+//    }
+//    else
+//    {
+//        string ters = " " + ar4[i];
+//        Console.WriteLine(ters);
+//    }
+//}
+#endregion
+#region task5
+//int[] ar5= { 3, 5, 8, 4, 7 };
+//string d="";
+//for (int i = 0; i < ar5.Length; i++)
+//{
+//    d+=ar5[i];
+
+//}
+//Console.WriteLine(d);
+#endregion
